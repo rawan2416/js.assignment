5778109149a2b68fe9eba214fac4c0123df6661c
rawan2416 <codebraver81@codebrave.org>
2025-11-04 22:51:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function names() {
+    name=prompt("What is your name?");
+    document.getElementById("changename").innerHTML="Hello "+name+"! Welcome to my page.";
+
+}
+function boxcolor() {
+    document.getElementById("changebox").style.backgroundColor="pink"; 
+}
